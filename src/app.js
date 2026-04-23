028ffc37731a9eb5e9ee67518e07b08307dbe7af
Gabriel Mendez <gabrielenrique904@gmail.com>
2026-04-23 23:11:15 +0000

    primeros cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import "bootstrap";
-import "./style.css";
+let pinta = ["♠", "♣", "♥", "♦"];
+let valores = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+// Esta función se ejecuta sola al cargar la página
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    // Tus variables de índice aleatorio
+    let randomPinta = Math.floor(Math.random() * pinta.length);
+    let randomValores = Math.floor(Math.random() * valores.length);
+
+    // Selección de los elementos del DOM
+    let elementoPintaTop = document.querySelector("#pinta-top");
+    let elementoValor = document.querySelector("#valor");
+    let elementoPintaBot = document.querySelector("#pinta-bot");
+    let container = document.querySelector("#card-container");
+
+    // Asignar el contenido aleatorio
+    let simbolo = pinta[randomPinta];
+    elementoPintaTop.innerHTML = simbolo;
+    elementoPintaBot.innerHTML = simbolo;
+    elementoValor.innerHTML = valores[randomValores];
+
+    // Cambiar color si es corazones o diamantes
+    if (simbolo === "♥" || simbolo === "♦") {
+        container.style.color = "red";
+    } else {
+        container.style.color = "black";
+    }
 };
